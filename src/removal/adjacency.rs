00d26999e3e9702e94d57f53d18e8dec6c1b3041
Ángel Javier Alonso <angel@ajalonso.com>
2022-04-17 21:06:07 +0200

    Use hash table

--- src/removal/adjacency.rs
+++ src/removal/adjacency.rs
@@ -1,4 +1,9 @@
+use std::collections::hash_map::Iter;
+use std::collections::HashMap;
+use std::hash::BuildHasherDefault;
+use std::iter::Peekable;
 use litemap::LiteMap;
+use nohash_hasher::NoHashHasher;
 
 use sorted_iter::assume::{AssumeSortedByItemExt, AssumeSortedByKeyExt};
 use sorted_iter::{SortedIterator, SortedPairIterator};
@@ -6,14 +11,16 @@ use sorted_iter::{SortedIterator, SortedPairIterator};
 use crate::edges::{BareEdge, FilteredEdge};
 use crate::CriticalGrade;
 
+pub(crate) type VertexAdjacency<G> = HashMap<usize, G, BuildHasherDefault<NoHashHasher<usize>>>;
+
 pub(crate) struct AdjacencyMatrix<G> {
-    matrix: Vec<LiteMap<usize, G>>,
+    pub(crate) matrix: Vec<VertexAdjacency<G>>,
 }
 
 impl<G: CriticalGrade> AdjacencyMatrix<G> {
     pub fn new(n_vertices: usize) -> Self {
         Self {
-            matrix: vec![LiteMap::new(); n_vertices],
+            matrix: vec![HashMap::with_hasher(BuildHasherDefault::default()); n_vertices],
         }
     }
 
@@ -37,8 +44,6 @@ impl<G: CriticalGrade> AdjacencyMatrix<G> {
     /// Returns an iterator over the open neighbours of the vertex u and the grade of the edge that
     /// connects u and its neighbor.
     /// The open neighbours of the vertex u are those that are connected by an edge.
-    ///
-    /// The returned iterator is sorted by vertex.
     pub fn open_neighbours(&self, u: usize) -> impl Iterator<Item = (usize, G)> + '_ {
         self.matrix[u]
             .iter()
@@ -51,12 +56,8 @@ impl<G: CriticalGrade> AdjacencyMatrix<G> {
     /// The closed neighbours of the vertex u are those that are connected by an edge that is either
     /// critical in the current graph, or whose index is equal to or less than max_index_value, in
     /// addition to u itself.
-    ///
-    /// The returned iterator is sorted by vertex.
     pub fn closed_neighbours(&self, u: usize, u_value: G) -> impl Iterator<Item = (usize, G)> + '_ {
-        self.open_neighbours(u)
-            .assume_sorted_by_item()
-            .union(std::iter::once((u, u_value)))
+        std::iter::once((u, u_value)).chain(self.open_neighbours(u))
     }
 
     fn common_neighbours_raw<'a>(
@@ -64,9 +65,10 @@ impl<G: CriticalGrade> AdjacencyMatrix<G> {
         edge: &'a FilteredEdge<G>,
     ) -> impl Iterator<Item = (usize, (G, G))> + 'a {
         let BareEdge(u, v) = edge.edge;
-        let neigh_u = self.open_neighbours(u).assume_sorted_by_key();
-        let neigh_v = self.open_neighbours(v).assume_sorted_by_key();
-        neigh_u.join(neigh_v)
+        CommonNeighboursIter {
+            u_adj_iter: self.matrix[u].iter(),
+            v_adj: &self.matrix[v],
+        }
     }
 
     pub fn common_neighbours<'a>(
@@ -82,14 +84,31 @@ impl<G: CriticalGrade> AdjacencyMatrix<G> {
         edge: &'a FilteredEdge<G>,
     ) -> impl Iterator<Item = (usize, G)> + 'a {
         let BareEdge(edge_u, edge_v) = edge.edge;
-        self.common_neighbours(edge)
-            .map(move |(neigh, neigh_value)| (neigh, neigh_value.join(&edge.grade)))
-            .assume_sorted_by_item()
-            .union(std::iter::once((edge_u, edge.grade.clone())))
-            .union(std::iter::once((edge_v, edge.grade.clone())))
+        std::iter::once((edge_u, edge.grade.clone()))
+            .chain(std::iter::once((edge_v, edge.grade.clone())))
+            .chain(self.common_neighbours(edge)
+                    .map(move |(neigh, neigh_value)| (neigh, neigh_value.join(&edge.grade)))
+            )
     }
 }
 
+struct CommonNeighboursIter<'a, G> {
+    u_adj_iter: Iter<'a, usize, G>,
+    v_adj: &'a VertexAdjacency<G>,
+}
+
+impl<'a, G: CriticalGrade> Iterator for CommonNeighboursIter<'a, G> {
+    type Item = (usize, (G, G));
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some((u_neigh, u_crit)) = self.u_adj_iter.next() {
+            if let Some(v_crit) = self.v_adj.get(u_neigh) {
+                return Some((*u_neigh, (u_crit.clone(), v_crit.clone())))
+            }
+        }
+        None
+    }
+}
 #[cfg(test)]
 mod tests {
     use crate::edges::BareEdge;
