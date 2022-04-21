4b6bb43329570bb54bea8b516dc947f2ea8d4364
Ángel Javier Alonso <angel@ajalonso.com>
2022-04-21 14:01:40 +0200

    Improve zip creation

--- create_zip.sh
+++ create_zip.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-zip -r code.zip Cargo.lock Cargo.toml src README.md
+rm -r target/doc
+rm -r doc
+cargo doc --no-deps
+cp -r target/doc .
+
+rm code.zip
+# shellcheck disable=SC2046
+zip -r code.zip clippy.toml Cargo.toml src doc $(git ls-files datasets) download_datasets.sh tests examples README.md
